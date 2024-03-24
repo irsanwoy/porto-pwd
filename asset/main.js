4099306c9cb352ce7c68b9503d5a5f09aba5c9fc
irsanwoy <irsanwoy54@gmail.com>
2024-03-25 01:46:19 +0700

    menit 15:50

---
+++ asset/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    String:["Mahasiswa","Single","Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    startDelay: 1000,
+    loop: true
+})
